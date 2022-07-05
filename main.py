7ca08b02390fd2bb0652c77f7ccd21a031a70b2d
Sleskrut <kirill5k@yandex.ru>
2022-07-05 22:54:19 +0300

    Merge branch 'main' of https://github.com/sleskrut/Progect-1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+import math
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -15,4 +15,26 @@ if __name__ == '__main__':
 print('gnhtyf')
 print('gnhtyf')
 print('1')
+print('2')
+num = 124
+print (num)
+b = int(num) + 5
+print(b)
+print(5 + True)
+print(4.2e3)
+x = 21
+y = 5
+print(x / y)
+print(x // y)
+math.ceil(4.2)
+a = 8;
+print(str(a))
+c = 'g \
+k'
+print(c)
+c = 'g \n' \
+'k'
+print(c)
+'Number = {}'.format(46)
+print(number)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
