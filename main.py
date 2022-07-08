1b0435dec91f2e1dbb4bdeb4e81db266441beaca
Sleskrut <kirill5k@yandex.ru>
2022-07-08 17:38:59 +0300

    Merge branch 'main' of https://github.com/sleskrut/Progect-1

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import math
+import functools
+@functools.lru_cache(maxsize= None)
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -53,6 +55,7 @@ for x in z:
     print('{} ^ 3 = {}'.format(x, x ** 3))
     if x == z[9]:
      break
+print('Информация по спискам удалена по ошибке')
 print('Условные конструкции')
 if z[8] == 9:
     print('a')
@@ -65,11 +68,11 @@ elif z[1] == 2:
     print('e')
 else:
     print('f')
-num = int(input('Введите номер'))
-if num == 0:
-    print('zero')
-else:
-    print('not zero')
+#num = int(input('Введите номер'))
+#if num == 0:
+   # print('zero')
+#else:
+    #print('not zero')
 print('Функции')
 def g(l):
     for i in range(l):
@@ -78,6 +81,54 @@ g(10)
 def h(j, m):
     return (j * m)
 print(h(4, 5))
+def red():
+    kl = 8
+    print(kl)
+    def fgg():
+        nonlocal kl
+        print(kl)
+        kl = kl + 1
+        print(kl)
+red()
+def factorial(n):
+    if n == 0:
+        return  1
+    else:
+        return n * factorial(n - 1)
+print(factorial(100))
+print('Циклические кострукции')
+t = ''
+a = ''
+while t != '  ':
+    t = input('print ' ' to close')
+    a = a + t
+else:
+    print('0')
+print(a)
+o = 3
+p = ''
+while o > 0:
+    o -= 1
+    p = input('Введите число от 1 до 10')
+    if p == '1':
+        print('Вы угадали число')
+        break
+    else:
+        print('Попытайтесь ещё раз. Осталость попыток ', o)
+
+else:
+    print('Вы не угадали число')
+for i in range(5, 10, 2):
+    print(i**3)
+for i in range(1,10, 1):
+    for j in range(1, 5, 1):
+        print('1', end='')
+    print()
+print('Python Starter completed')
+
+
+
+
 
 
 
